297bf4f4483c44d626c7129f0b7c6c78e3d2b19e
Paul Starodub <amorallex@gmail.com>
2023-02-16 13:09:03 +0400

    Updated vievs.py

--- app_list/views.py
+++ app_list/views.py
@@ -1,8 +1,12 @@
 from django.views import generic
 
-from app_list.models import Task
+from app_list.models import Task, Tag
 
 
 class IndexView(generic.ListView):
     queryset = Task.objects.prefetch_related("tags")
     template_name = "app_list/index.html"
+
+
+class TagView(generic.ListView):
+    queryset = Tag.objects.prefetch_related("tasks")
