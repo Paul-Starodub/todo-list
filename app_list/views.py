69ffb7bbd2a2041e327e6ed0b45e6de8292a3920
Paul Starodub <amorallex@gmail.com>
2023-02-16 17:47:07 +0400

    Updated views.py with change status of task

--- app_list/views.py
+++ app_list/views.py
@@ -1,4 +1,6 @@
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from app_list.models import Task, Tag
@@ -51,3 +53,18 @@ class TagDeleteView(generic.DeleteView):
 
     model = Tag
     success_url = reverse_lazy("app_list:tag-list")
+
+
+class TagTaskListView(generic.ListView):
+
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("app_list:index")
+
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs["pk"])
+        task.is_done = not task.is_done
+        task.save()
+        return HttpResponseRedirect(
+            reverse("app_list:index")
+        )
