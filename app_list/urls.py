c4539641529469bd88b34330d928119915ffcffa
Paul Starodub <amorallex@gmail.com>
2023-02-16 15:33:47 +0400

    Updated app_list/urls.py with pathes for tag

--- app_list/urls.py
+++ app_list/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from app_list.views import IndexView, TagView
+from app_list.views import (
+    IndexView,
+    TagView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
 
 urlpatterns = [
     path(
@@ -12,7 +18,23 @@ urlpatterns = [
         "tags/",
         TagView.as_view(),
         name="tag-list"
-    )
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create"
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="task-delete"
+    ),
+
 ]
 
 app_name = "app_list"
