e981943f92b8075d506af9563477cf7bb97652b5
Paul Starodub <amorallex@gmail.com>
2023-02-16 15:36:06 +0400

    Updated views.py with classes for tag

--- app_list/views.py
+++ app_list/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from app_list.models import Task, Tag
@@ -10,3 +11,25 @@ class IndexView(generic.ListView):
 
 class TagView(generic.ListView):
     queryset = Tag.objects.prefetch_related("tasks")
+
+
+class TagCreateView(generic.CreateView):
+
+    model = Tag
+    fields = "__all__"
+    template_name = "app_list/tag_form.html"
+    success_url = reverse_lazy("app_list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+
+    model = Tag
+    fields = "__all__"
+    template_name = "app_list/tag_form.html"
+    success_url = reverse_lazy("app_list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+
+    model = Tag
+    success_url = reverse_lazy("app_list:tag-list")
