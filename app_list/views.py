399f96123fa69517f145a7bff683cc9e3452cdfb
Paul Starodub <amorallex@gmail.com>
2023-02-16 10:54:48 +0400

    Updated views.py for home page

--- app_list/views.py
+++ app_list/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from app_list.models import Task
+
+
+class IndexView(generic.ListView):
+    queryset = Task.objects.all()
+    template_name = "app_list/index.html"
